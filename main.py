951e9e7e00b1550801d6b38e3e618386ef8b2c2e
SkyHampton <forresthampton826@gmail.com>
2022-05-31 20:25:01 -0700

    Reduced redundancy, fixed logic issues

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 from pytube import YouTube
 import os
 
+# get input until user types exit or quit
 while True:
+    # video url
     url = input("Enter the link to the youtube video, or quit/exit to close the program:\n")
     if url.lower() == "exit" or url.lower() == "quit":
         break
-    
+
+    # get mp4/mp3 from user, keeps asking until they enter mp3 or mp4
     while True:
         format = input("Enter mp4 or mp3 for the download format:\n").lower()
         if format == "mp4":
             mp4 = True
             break
         elif format == "mp3":
-            mp3 = True
+            mp4 = False
             break
         else:
             print("Incorrect format type.")
 
-
+    # get youtube video
     yt = YouTube(url)
     
     if mp4:
-        ordered = yt.streams.order_by('resolution')
-        filtered = ordered.filter(progressive=True, file_extension="mp4")
+        # order by resolution and get mp4 videos with both video and audio components
+        video = yt.streams.get_highest_resolution()
     else:
-        filtered = ordered.filter(only_audio=True)
+        video = yt.streams.get_audio_only()
     
-    sorted = filtered.desc()
-    first = sorted.first()
     path = os.getcwd() + "\\Downloads"
-    file = first.download(output_path = path)
+    file = video.download(output_path = path)
     if not mp4:
         name, ext = os.path.splitext(file)
         newFileName = name + ".mp3"
